1db8e14eeb0ed46f6a0576b37a1f1148abc71f05
aseerinet <44622173+aseerinet@users.noreply.github.com>
2026-01-25 07:51:58 +0300

    Update utils.ts 25 1 2026

--- utils.ts
+++ utils.ts
@@ -11,7 +11,7 @@ export const formatCurrency = (amount: number) => {
 };
 
 export const formatDate = (timestamp: number) => {
-  return new Intl.DateTimeFormat('ar-SA', {
+  return new Intl.DateTimeFormat('en-SA', {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
